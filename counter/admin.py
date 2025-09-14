bcfa2b879f9bd4be304a95fe09c86e68eb9126fd
ww24e018 <ww24e018@technikum-wien.at>
2025-09-14 15:17:24 +0200

    vibe admin

--- counter/admin.py
+++ counter/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import FoodItemTemplate, FoodItemLog
 
-# Register your models here.
+@admin.register(FoodItemTemplate)
+class FoodItemTemplateAdmin(admin.ModelAdmin):
+    list_display = ('name', 'modifier', 'joules')
+    list_filter = ('name',)
+    search_fields = ('name', 'modifier')
+
+@admin.register(FoodItemLog)
+class FoodItemLogAdmin(admin.ModelAdmin):
+    list_display = ('template', 'eaten_at')
+    list_filter = ('eaten_at',)
+    date_hierarchy = 'eaten_at'
