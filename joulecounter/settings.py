2105718e04f6ae6f87ca1bc4a03ee430b5678e90
ww24e018 <ww24e018@technikum-wien.at>
2025-09-14 18:08:31 +0200

    trying stuff

--- joulecounter/settings.py
+++ joulecounter/settings.py
@@ -128,7 +128,7 @@ LOGIN_URL = "/jc/admin/login/"
 LOGIN_REDIRECT_URL = "/jc/"
 STATIC_URL = '/jc/static/'
 
-CSRF_TRUSTED_ORIGINS = [os.getenv('CSRF_TRUSTED_ORIGINS','').split(',')]
+CSRF_TRUSTED_ORIGINS = [os.getenv('CSRF_TRUSTED_ORIGINS').split(',')]
 CSRF_COOKIE_SECURE = False
 
 # by vibe
